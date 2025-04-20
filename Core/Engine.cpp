318f6a6a4cf7f0a05f978bbb321a12e9b9c35288
Henry Nguyen <henry17080@gmail.com>
2025-04-20 15:25:41 +0700

    Engine done!

--- Core/Engine.cpp
+++ Core/Engine.cpp
@@ -1,11 +1,49 @@
 #include "Engine.h"
 
-Engine::Engine()
+bool Engine::Init()
 {
-    //ctor
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        SDL_Log("Failed to Initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+    //Create Window
+    window = SDL_CreateWindow("Henry's Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr)
+    {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return false;
+    }
+    //Create Renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        return false;
+    }
+
+    running = true;
+    return true;
 }
 
-Engine::~Engine()
+void Engine::HandleEvents()
 {
-    //dtor
+
+}
+
+void Engine::Update()
+{
+
+}
+
+void Engine::Render()
+{
+
+}
+
+void Engine::Clean()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
--- Core/Engine.h
+++ Core/Engine.h
@@ -1,16 +1,28 @@
 #ifndef ENGINE_H
 #define ENGINE_H
+#include "SDL.h"
 
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
 
 class Engine
 {
     public:
+        Engine* m_Engine;
         Engine();
-        virtual ~Engine();
 
-    protected:
+        bool Init();
+        bool IsRunning() { return running; }
+        void HandleEvents();
+        void Update();
+        void Render();
+        void Clean();
 
-    private:
+        bool running;
+
+        SDL_Window* window = nullptr;
+        SDL_Renderer* renderer = nullptr;
+
 };
 
 #endif // ENGINE_H
