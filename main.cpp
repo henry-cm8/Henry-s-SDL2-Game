e4125132b6c8a955d3abf5c0f11223ca29f2b3b7
Henry Nguyen <henry17080@gmail.com>
2025-04-20 14:49:35 +0700

    another one

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
-#include "SDL.h"
 #include <iostream>
+#include "SDL.h"
 
-using namespace std;
-
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
     std::cout << "Hello world!" << std::endl;
     return 0;
